1d7820c958a62c556b544d2106cf3d3652206de6
Jeffrey Liang <liang01@csu.fullerton.edu>
2020-11-05 17:53:37 -0800

    added vector tests - not complete yet

--- VectorTest.cpp
+++ VectorTest.cpp
@@ -39,6 +39,62 @@ int main()
     assert(v4[4] == 5);
     assert(v4.GetSize() == 5);
 
+    // Member functions
+
+    // 1. GetSize() passes constructor tests above
+
+    // 2. SetElement
+    v5.SetElement(0, 42);
+    v5.SetElement(4, 108);
+    assert(v5[0] == 42);
+    assert(v5[4] == 108);
+
+    // 3. Append
+    v1.Append(1);
+    assert(v1.GetSize() == 1);
+    assert(v1[0] == 1);
+
+    v4.Append(6);
+    v4.Append(7);
+    assert(v4.GetSize() == 7);
+    assert(v4[5] == 6);
+    assert(v4[6] == 7);
+
+    // 4. GetElement
+    assert(v2.GetElement(0) == 0);
+    assert(v4.GetElement(6) == v4[6]);
+
+    // Operator Overloading
+
+    // 1. Indexing [] operator: Passed above tests
+
+    // 2. Copy Assignment operator =
+    v5 = v1;
+    assert(v5[0] == v1[0]);
+    assert(v5.GetSize() == 1);
+    
+    v5 = v4;
+    assert(v5.GetSize() == 7);
+    assert(v5[0] == 1);
+    assert(v5[4] == 5);
+    assert(v5[6] == 7);
+
+    // 3. Binary + Operator
+    Vector<int> v7(v1);
+    v7 = v7 + v1;
+    assert(v7[0] == 2);
+
+    Vector<int> v6(7);
+    v6 = v5 + v4;
+    assert(v6.GetSize() == v4.GetSize());
+    assert(v6[0] == 2);
+    assert(v6[1] == 4);
+    assert(v6[6] == 14);
+
+    // 4. Binary - Operator
+    Vector<int> v8(v1);
+    v8 = v8 - v1;
+    assert(v8[0] == 0);
 
     std::cout << "Testing completed." << std::endl;
     return 0;
